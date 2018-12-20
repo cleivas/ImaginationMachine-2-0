5ca868e929fce1e9805e457322ad22d8e1bd7cef
cleivas <clara@guildisgood.com>
2018-12-20 01:54:13 -0800

    Add files via upload

---
+++ Arduino - peristaltic pumps/IM_001_drop/IM_001_drop.ino
@@ -0,0 +1,32 @@
+int red = 11;    
+int purple = 10;
+int blue = 9;
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+ //analogWrite(red, 255);
+ //analogWrite(purple, 255);
+ //analogWrite(blue, 255);
+}
+
+void drop(int _p){
+  int p = _p;
+  int a = 160;
+  if(p == blue) a = 180;
+  else if(p == purple) a = 160;
+  analogWrite(p, a);
+  delay(120);
+  analogWrite(p, 0);
+}
+
+void serialEvent() {
+    char inByte = (char)Serial.read();
+
+   if(inByte == 'r') drop(red);
+   else if(inByte == 'p') drop(purple);
+   else if(inByte == 'b') drop(blue);
+}
